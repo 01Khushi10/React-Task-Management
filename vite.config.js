cd7b2f7561be3151cea385b14d89401d30850225
01Khushi10 <khushivm123@gmail.com>
2025-03-27 15:15:28 +0530

    module.export changed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+  ],
     base: process.env.VITE_BASE_PATH || '/React-Task-Management'
 })
 
